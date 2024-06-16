90eaa1c9b40e4e2d586b31c0df657d143adebdb0
acferlucas <acferlucas.dev@gmail.com>
2024-06-16 18:41:46 -0300

    upload some assets

--- web/src/pages/_layout/auth.tsx
+++ web/src/pages/_layout/auth.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from 'react-router-dom'
 
+import asideImage from '../../assets/example1.jpg'
+
 export function AuthLayout() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <Outlet />
       <div className="hidden bg-muted lg:block">
         <img
-          src="/placeholder.svg"
+          src={asideImage}
           alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="h-screen w-screen object-fill dark:brightness-[0.2] dark:grayscale"
         />
       </div>
     </div>
